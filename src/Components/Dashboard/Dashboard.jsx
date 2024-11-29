df4885b4652e21d0eb6138167ca2b0e2d45a3b28
SafaSELLAM <safa.sellam@outlook.fr>
2024-11-29 10:59:36 +0100

    ADD dashboard component

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,19 @@
+import { UserName } from "../userName/UserName";
+import { LeftBanner } from "../LeftBanner/LeftBanner";
+import { Content } from "../Content/Content.jsx";
+import './dashboard.css'
+
+export const Dashboard = () => {
+    const userId = Number(import.meta.env.VITE_APP_USERID)
+
+    return (
+        <div className="row ">
+            <LeftBanner />
+            <div className="dashboardContent">
+                <UserName userId={userId} />
+                <Content userId={userId} />
+            </div>
+        </div>
+    );
+};
+
