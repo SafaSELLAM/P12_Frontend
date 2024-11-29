c9c28fba50d0ab4397f282a10cafdbdced5d832e
SafaSELLAM <safa.sellam@outlook.fr>
2024-11-29 10:59:03 +0100

    Fetch user name

---
+++ src/Components/UserName/UserName.jsx
@@ -0,0 +1,41 @@
+import { getUserById } from "../../fetchAPI.js";
+import { useState, useEffect } from "react";
+import './username.css'
+
+export const UserName = ({ userId }) => {
+    const [userName, setUserName] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const userData = await getUserById(userId);
+                setUserName(userData.firstName);
+
+
+            } catch (error) {
+                setError('Error loading datas.');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchUserData();
+    }, [userId]);
+
+    if (loading) {
+        return <p>Chargement...</p>;
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    return (
+        <div className="userContent column ">
+            <h1>Bonjour <span className="colorPrimary">{userName}</span></h1>
+            <p>Félicitation ! Vous avez explosé vos objectifs hier 👏 </p>
+        </div>
+    )
+};
