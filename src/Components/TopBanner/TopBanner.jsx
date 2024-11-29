bbd4662c5f5945a1f4061cc6a91ada9bb6a24987
SafaSELLAM <safa.sellam@outlook.fr>
2024-11-29 10:56:08 +0100

    Add top banner

--- src/Components/TopBanner/TopBanner.jsx
+++ src/Components/TopBanner/TopBanner.jsx
@@ -1,16 +1,17 @@
 import logo from "../../assets/sportsee_logo.png";
 import "./topbanner.css"
-export const TopBanner = () => {
-    return (
-        <header className="backSecondary center">
-            <img src={logo} alt="sportsee logo" />
-            <ul className="noList row around listHeader">
-                <li><a href="#" className="colorTertiary back">Accueil</a></li>
-                <li><a href="#" className="colorTertiary">Profil</a></li>
-                <li><a href="#" className="colorTertiary">Réglage</a></li>
-                <li><a href="#" className="colorTertiary">Communauté</a></li>
-            </ul>
-        </header>
-    )
 
-}
+const menuItems = ["Accueil", "Profil", "Réglage", "Communauté"];
+
+export const TopBanner = () => (
+    <header className="backSecondary center">
+        <img src={logo} alt="sportsee logo" />
+        <ul className="noList row around listHeader">
+            {menuItems.map((item, index) => (
+                <li key={index}>
+                    <a href="#" className="colorTertiary noDecoration">{item}</a>
+                </li>
+            ))}
+        </ul>
+    </header>
+)
