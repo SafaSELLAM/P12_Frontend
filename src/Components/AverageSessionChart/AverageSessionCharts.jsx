fe286596973e0e8a63679c2031cfef04b46d9d19
SafaSELLAM <safa.sellam@outlook.fr>
2024-12-19 20:32:53 +0100

    add hover control + adjust Yaxis + Add title and style on cursor

--- src/Components/AverageSessionChart/AverageSessionCharts.jsx
+++ src/Components/AverageSessionChart/AverageSessionCharts.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
-import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { useEffect, useState, useRef } from 'react';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import './averagesession.css'
 import { getUserAverageSessions } from '../../fetchAPI.js';
 
 export const AverageSessionCharts = ({ userId }) => {
     const [sessions, setSessions] = useState([])
-
+    const refChartContainer = useRef(0)
     useEffect(() => {
         const fetchSessions = async () => {
             try {
@@ -29,19 +29,41 @@ export const AverageSessionCharts = ({ userId }) => {
         }
         return null;
     };
-
+    function handleHover(position) {
+        const divBackgroundChange = refChartContainer.current.current
+        if (position.isTooltipActive) {
+            const windowWidth = divBackgroundChange.clientWidth
+            const mouseXpercentage = Math.round((position.activeCoordinate.x / windowWidth) * 100)
+            divBackgroundChange.style.background = `linear-gradient(90deg, rgba(255,0,0,1) ${mouseXpercentage}%, rgba(175,0,0,0.5) ${mouseXpercentage}%, rgba(175,0,0,0.5) 100%)`
+        } else if (!position.mouseover) {
+            divBackgroundChange.style.background = `none`
+        }
+    }
     return (
         <div className="sessionChart">
-            <ResponsiveContainer width="100%" height="100%">
-                <LineChart data={sessions}>
+            <ResponsiveContainer width="100%" height="100%" ref={refChartContainer} fill="#FF0D0D">
+                <LineChart data={sessions} onMouseMove={handleHover} margin={{ top: 0, right: 0, bottom: 10, left: 5 }}>
+                    <text style={{
+                        fill: '#ffffff80',
+                        fontSize: '16px',
+                        fontWeight: 'bold',
+                    }}>
+                        <tspan className="sessionChartTitle" x={30} y={35}>
+                            Durée moyenne des
+                        </tspan>
+                        <tspan className="sessionChartTitle" x={30} y={55}>
+                            sessions
+                        </tspan>
+                    </text>
                     <XAxis
                         dataKey="day"
-                        tick={{ fill: '#ffffff80' }}
+                        tick={{ fill: '#ffffff91' }}
                         tickLine={false}
                         axisLine={false}
-                        padding={{ left: 30, right: 30 }}
 
                     />
+                    <YAxis domain={['dataMin-3', 'dataMax+20']} hide />
+
                     <Tooltip content={CustomTooltip} />
                     <Line
                         type="monotone"
@@ -49,9 +71,13 @@ export const AverageSessionCharts = ({ userId }) => {
                         stroke="#ffffff80"
                         strokeWidth={3}
                         dot={false}
-                        activeDot={{ stroke: '#fff', strokeWidth: 2 }}
-                    />
+                        activeDot={{
+                            stroke: "rgba(255,255,255,0.8)",
+                            strokeWidth: 10,
+                            r: 5,
+                        }}
 
+                    />
                 </LineChart>
             </ResponsiveContainer>
         </div>
